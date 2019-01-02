c24ec4c942d124dc375fcb5825d9c010f84ac855
inga.pabreze@gmail.com <inga.pabreze@gmail.com>
2019-01-02 22:59:42 +0200

    03php

---
+++ index03.php
@@ -0,0 +1,14 @@
+<?php
+    $x= 10;
+    $y= 7;
+    $result= $x+$y;
+?>
+<p><?php echo "$x + $y = $result"; ?></p>
+<?php $result= $x-$y; ?>
+<p><?php echo "$x - $y = $result"; ?></p>
+<?php $result= $x*$y; ?>
+<p><?php echo "$x * $y = $result"; ?></p>
+<?php $result= $x/$y; ?>
+<p><?php echo "$x / $y = $result"; ?></p>
+<?php $result= $x%$y; ?>
+<p><?php echo "$x % $y = $result"; ?></p>
