d1803a4317d78a9fcd1966e8df7eeacb2d20a8f6
inga.pabreze@gmail.com <inga.pabreze@gmail.com>
2019-01-02 20:52:58 +0200

    php01

---
+++ index01.php
@@ -0,0 +1,10 @@
+<?php
+    $country= 'Lietuva';
+    $city= 'Vilnius';
+    $population=574221;
+?>
+<ul>
+    <li><?php echo 'Šalis - '.$country; ?></li>
+    <li><?php echo 'Miestas - '.$city; ?></li>
+    <li><?php echo 'Populiacija - '.$population; ?></li>
+</ul>
