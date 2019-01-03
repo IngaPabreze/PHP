a11395d103e4576e1049788f6ed2bf81f9d7165e
inga.pabreze@gmail.com <inga.pabreze@gmail.com>
2019-01-03 18:02:39 +0200

    pakoreguota

--- index01.php
+++ index01.php
@@ -3,8 +3,22 @@
     $city= 'Vilnius';
     $population=574221;
 ?>
-<ul>
-    <li><?php echo 'Šalis - '.$country; ?></li>
-    <li><?php echo 'Miestas - '.$city; ?></li>
-    <li><?php echo 'Populiacija - '.$population; ?></li>
-</ul>
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <ul>
+        <li><?php echo 'Šalis - '.$country; ?></li>
+        <li><?php echo 'Miestas - '.$city; ?></li>
+        <li><?php echo 'Populiacija - '.$population; ?></li>
+    </ul>
+
+</body>
+</html>
+
+
