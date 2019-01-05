291a79ed40d8ee6152ed902f4377b954af418259
inga.pabreze@gmail.com <inga.pabreze@gmail.com>
2019-01-05 15:17:55 +0200

    php12

---
+++ index12.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <?php
+        if(isset($_POST['name'])){
+            if($_POST['name'] && $_POST['surname']){
+                echo  $_POST['name'].' '.$_POST['surname'];
+                }else{
+                echo 'Įveskite vardą ir pavardę!';
+                }
+        }else{
+            ?>
+                <form action ="<?php $_PHP_SELF ?>"; method="post">
+                    <p> Vardas: <input type="text" name="name"></p>
+                    <p> Pavardė: <input type="text" name="surname"></p>
+                    <input type="submit" value="Send" name="mygtukas">
+                </form>
+            <?php
+        };
+   ?>
+
+</body>
+</html>
